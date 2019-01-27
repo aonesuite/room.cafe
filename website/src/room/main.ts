36bdab71b579ec3c2b8596d4393c559d676ac509
Miclle Zheng <miclle.zheng@gmail.com>
2019-01-28 01:15:40 +0800

    Update home page

--- website/src/room/main.ts
+++ website/src/room/main.ts
@@ -1,12 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from '@/store'
-import router from './router'
+import '@/assets/main.scss'
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import Vuex from 'vuex';
+import BootstrapVue from 'bootstrap-vue';
+
+
+import App from './App.vue';
+import store from '@/store';
+import router from './router';
+
+import Icon from "@/components/icons/Icon.vue";
+
+Vue.component(Icon.name, Icon);
+
+Vue.use(Vuex);
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
 new Vue({
   store,
   router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
