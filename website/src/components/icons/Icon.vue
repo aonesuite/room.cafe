50eca8cd2933bfcf2acb41a4d6aaf5b25cc2f059
Miclle Zheng <miclle.zheng@gmail.com>
2019-01-28 01:14:43 +0800

    Add icon components

---
+++ website/src/components/icons/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <component :class="`icon-${type}`" v-bind:is="currentIcon"></component>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+import Video from "./IconVideo.vue" // 摄像头
+import IconChalkboard from "./IconChalkboard.vue" // 黑板
+import IconCommentAltLines from "./IconCommentAltLines.vue" // 黑板
+
+const icons: any = {
+  "video": Video,
+  "chalkboard": IconChalkboard,
+  "comment-alt-lines": IconCommentAltLines,
+}
+
+@Component
+export default class Icon extends Vue {
+  @Prop() private type!: string;
+
+  currentIcon = icons[this.type];
+}
+</script>
