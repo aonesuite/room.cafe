26eb1dab878db10f4efc3fb4d612ac9def25ffba
Miclle Zheng <miclle.zheng@gmail.com>
2019-02-07 18:31:49 +0800

    Add `dev.room.cafe` SSL certificate

--- website/vue.config.js
+++ website/vue.config.js
@@ -6,7 +6,11 @@ module.exports = {
     open: process.platform === 'darwin',
     host: 'dev.room.cafe',
     port: 3000,
-    hotOnly: true
+    hotOnly: true,
+    https: {
+      key: fs.readFileSync('../certificate/dev.room.cafe.key'),
+      cert: fs.readFileSync('../certificate/dev.room.cafe.crt')
+    }
   },
 
   // https://cli.vuejs.org/zh/config/#pages
