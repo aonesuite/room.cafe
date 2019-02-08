5ae5ebd9623dee345f15648972cb02a47c98cf9a
Miclle Zheng <miclle.zheng@gmail.com>
2019-02-08 18:00:46 +0800

    Add stream model

---
+++ Website/src/types/stream.ts
@@ -0,0 +1,79 @@
+import remove from "lodash.remove";
+import EventEmitter from "wolfy87-eventemitter";
+import * as QNRTC from "pili-rtc-web";
+import { RTCUser } from '@/types/user';
+
+export class Stream extends EventEmitter {
+
+  public trackList: QNRTC.Track[] = [];
+  private direction: "send" | "recv";
+
+  public tag?: string;
+
+  public user?: RTCUser;
+
+  /**
+   * 用于标记这个流是否调用了 release 方法，即表示这个流已经被释放
+   */
+  public isDestroyed: boolean = false;
+
+  public audioTrack?: QNRTC.Track;
+  public videoTrack?: QNRTC.Track;
+
+  public constructor(tracks: QNRTC.Track[], direction: "send" | "recv" = "send", user: QNRTC.User, tag?: string) {
+    super();
+
+    this.direction = direction;
+    this.user = user;
+    this.tag = tag;
+
+    tracks.forEach(track => {
+      this.trackList.push(track);
+      if (this.tag === "" || this.tag === undefined) this.tag = track.info.tag || "master";
+      if (track.info.kind === "video") this.videoTrack = track;
+      if (track.info.kind === "audio") this.audioTrack = track;
+
+      track.on("release", () => {
+        // 从 track list 中移除
+        remove(this.trackList, t => t === track);
+
+        if (track.info.kind === "video") this.videoTrack = undefined;
+        if (track.info.kind === "audio") this.audioTrack = undefined;
+
+        // 如果自己所有的 track 都 release 了，自己也 release
+        if (this.trackList.length === 0) {
+          this.release();
+        }
+      })
+    });
+  }
+
+  public addTracks(tracks: QNRTC.Track[]): void {
+    for (const track of tracks) {
+      const index = this.trackList.findIndex(t => t.info.trackId === track.info.trackId);
+      if (index >= 0) continue;
+      this.trackList.push(track);
+      if (track.info.kind === "video") this.videoTrack = track;
+      if (track.info.kind === "audio") this.audioTrack = track;
+    }
+  }
+
+  /**
+   * 释放这个流下的所有资源，包括流和用于播放流的媒体元素
+   */
+  public release(): void {
+    if (this.isDestroyed) {
+      return;
+    }
+
+    for (let i = 0; i < this.trackList.length; i += 1) {
+      const track = this.trackList[i];
+      track.release();
+    }
+
+    this.trackList = [];
+    this.isDestroyed = true;
+    this.emit("release");
+    this.removeEvent();
+  }
+}
