e1c30dee4b4e7eb7f9a4690ea97caa3f4e0090bc
Miclle Zheng <miclle.zheng@gmail.com>
2019-02-08 18:00:38 +0800

    Add rtc user model

--- website/src/types/user.ts
+++ website/src/types/user.ts
@@ -1,8 +1,11 @@
 /*
- * Created on Sat Feb 02 2019
- * Copyright (c) 2019 Miclle Zheng <miclle.zheng@gmail.com>
- * Distributed under terms of the MIT license.
- */
+* Created on Sat Feb 02 2019
+* Copyright (c) 2019 Miclle Zheng <miclle.zheng@gmail.com>
+* Distributed under terms of the MIT license.
+*/
+
+import * as QNRTC from 'pili-rtc-web';
+
 export interface UserArgs {
   name: string;
   email?: string;
@@ -31,4 +34,30 @@ export class User {
     this.gender = options.gender;
     this.avatar = options.avatar;
   }
+}
+
+export class RTCUser extends QNRTC.User {
+
+  public userId: string;
+  public id?: number;
+  public name?: string;
+  public email?: string;
+  public gender?: string;
+  public avatar?: string;
+
+  public constructor(userId: string, userData?: string){
+    super(userId, userData);
+
+    this.userId = userId;
+
+    if (userData !== undefined) {
+      var user = new User(JSON.parse(userData));
+      this.id = user.id;
+      this.name = user.name;
+      this.email = user.email;
+      this.gender = user.gender;
+      this.avatar = user.avatar;
+    }
+  }
+
 }
