326aa125c195a1c4272e0cdb844b7908c1d6c00b
Miclle Zheng <miclle.zheng@gmail.com>
2019-02-10 00:48:14 +0800

    Cleanup room stream component

--- Website/src/components/room/Streams.vue
+++ Website/src/components/room/Streams.vue
@@ -11,8 +11,8 @@
 <script lang="ts">
 import Vue from 'vue';
 import { mapState, mapActions } from 'vuex';
-import * as QNRTC from 'pili-rtc-web'
-import * as Clarity from '../../constants/clarity'
+import * as QNRTC from 'pili-rtc-web';
+import * as Clarity from '../../constants/clarity';
 import Monitor from './Monitor.vue';
 
 export default Vue.extend({
@@ -33,14 +33,6 @@ export default Vue.extend({
   },
 
   methods: {
-    ...mapActions("user", [
-      'fetchState'
-    ]),
-
-    ...mapActions("room", [
-      "createRoom"
-    ]),
-
     // 发布本地音视频
     async publish() {
       const clarity = Clarity.getClarity("HD");
@@ -56,23 +48,10 @@ export default Vue.extend({
           height: clarity.height,
           bitrate: clarity.bitrate,
         }
-      })
+      });
 
-      tracks.map(track => track.setMaster(true))
-
-      await this.RTC.publish(tracks)
-
-      // eslint-disable-next-line
-      console.log("publish tracks", tracks);
-      /* eslint-disable */
-
-      // const containerElement = document.getElementById(`user_${this.user.id}`);
-      // if (containerElement === null) return;
-
-      // for (const track of tracks) {
-      //   if (track.info.tag === "audio") continue;
-      //   track.play(containerElement, true);
-      // }
+      tracks.map(track => track.setMaster(true));
+      await this.RTC.publish(tracks);
     }
   },
 
