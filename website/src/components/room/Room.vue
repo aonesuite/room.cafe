0d414d30116a71b6320043703264ff9bdc6cf636
Miclle Zheng <miclle.zheng@gmail.com>
2019-02-16 03:33:27 +0800

    Add allow devices modal

--- website/src/components/room/Room.vue
+++ website/src/components/room/Room.vue
@@ -8,9 +8,11 @@
 
     <Navbar />
 
-    <div class="hint-exited" v-if="RTC.exited">
-      <p>You left the interact room.</p>
-      <b-btn variant="success" @click="joinRoom">Reenter the room</b-btn>
+    <div class="hint">
+      <div class="hint-exited" v-if="RTC.exited">
+        <p>You left the interact room.</p>
+        <b-btn variant="success" @click="joinRoom">Reenter the room</b-btn>
+      </div>
     </div>
 
     <WhiteBoard v-if="RTC.roomState === 2 && roomInfo.whiteboard_id && roomInfo.whiteboard_token" />
@@ -18,6 +20,22 @@
     <Streams v-if="RTC.roomState === 2" />
 
     <QuickStartModal @joinRoom="joinRoom" />
+
+    <b-modal
+      id="AllowDevices"
+      ref="AllowDevices"
+      centered
+      :lazy="true"
+      :no-close-on-backdrop="true"
+      size="lg"
+      hide-footer
+      hide-header
+      class="modal-allow-devices">
+      <div class="hint-allow">
+        <h5>Allow Room Cafe to use your camera and microphone</h5>
+        <p>Room Cafe needs access to your camera and microphone so that other participants can see and hear you. Room Cafe will ask you to confirm this decision on each browser and computer you use.</p>
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -74,6 +92,8 @@ export default Vue.extend({
 
   async created () {
     await this.fetchState();
+
+    this.$root.$emit('bv::show::modal', 'AllowDevices');
 
     if (this.signedIn) {
       if (this.$route.name === 'room-quick-start') {
