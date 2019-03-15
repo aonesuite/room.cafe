3ac063660f5c2b858088a1c0c8fd71a08a5470a8
Miclle Zheng <miclle.zheng@gmail.com>
2019-03-15 20:36:35 +0800

    Update chat & share hint i18n

--- website/src/locales/index.ts
+++ website/src/locales/index.ts
@@ -35,9 +35,16 @@ export declare type Message = {
   // Room
   invite_people: string;
   whiteboard: string;
+
   copy_link_to_share: string;
+  copy_link_success: string;
+  copy_link_error: string;
+
   close_chat: string;
   open_chat: string;
+  chat: string;
+  placeholder_send_message: string;
+
   share_screen: string;
   stop_share_screen: string;
   microphone_open: string;
