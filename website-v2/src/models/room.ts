e6e54a37acae324615c1ba69571b79e60aa1e339
Miclle Zheng <miclle.zheng@gmail.com>
2020-04-17 01:52:22 +0800

    Add rtn, whiteboard type & api

--- website-v2/src/models/room.ts
+++ website-v2/src/models/room.ts
@@ -23,3 +23,16 @@ export interface IRoomInfo {
   created_at:       number
   updated_at:       number
 }
+
+export interface IRTN {
+  app_id:    string // APP ID
+	channel:   string // 目标频/房间名称，保持跟 Room UUID 一致
+	uid:       number // 用户 ID
+	rtc_token: string // RTC RoomToken
+	rtm_token: string // RTM RoomToken
+}
+
+export interface IWhiteboard {
+	whiteboard_id:    string // 白板房间 ID
+	whiteboard_token: string // 白板房间 token
+}
