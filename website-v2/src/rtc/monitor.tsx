b1bbb09cdd583717521235b9c65e92a8b75f049c
Miclle Zheng <miclle.zheng@gmail.com>
2020-04-17 23:26:55 +0800

    Add attendee info to rtc monitor

--- website-v2/src/rtc/monitor.tsx
+++ website-v2/src/rtc/monitor.tsx
@@ -1,32 +1,55 @@
-import React, { useEffect, useRef } from "react"
+import React, { useState, useRef, useEffect } from "react"
 import { observer } from "mobx-react-lite"
 import className from "classnames"
 
-import { User } from "models"
+import { User, IAttendees } from "models"
 import AudioVolume from "./AudioVolume"
 import { ReactComponent as MicrophoneSlashSVG } from "assets/icons/MicrophoneSlash.svg"
+
+import { useRoomStore } from "room/context"
 
 interface IMonitorOptions {
   user: User
 }
 
 const Monitor = observer((options: IMonitorOptions) => {
-
-  const monitorRef = useRef<HTMLDivElement>(null)
+  const { roomStore } = useRoomStore()
+  const [attendee, setAttendee] = useState<IAttendees | undefined>(undefined)
+  const playerRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const monitorElement = monitorRef.current
-    if (!monitorElement) return
-
+    const playerElement = playerRef.current
+    if (!playerElement) return
     if (!options.user.videoTrack) return
-    options.user.videoTrack.play(monitorElement)
+    options.user.videoTrack.play(playerElement)
   }, [options.user.videoTrack])
 
-  return (
-    <div ref={monitorRef} id={`monitor-${options.user.uid}`} className="monitor">
-      <div className="info">
-        { options.user.uid }
+  useEffect(() => {
+    setAttendee(roomStore.attendees?.find(item => item.uid === options.user.uid))
+  }, [options.user.uid, roomStore.attendees])
 
+  return (
+    <div id={`monitor-${options.user.uid}`} className="monitor">
+      <svg role="img" viewBox="0 0 16 9" xmlns="http://www.w3.org/2000/svg"></svg>
+
+      <div className="cover">
+        {
+          attendee && attendee?.avatar &&
+          <img className="avatar" src={attendee?.avatar} />
+        }
+      </div>
+
+      <div
+        ref={playerRef}
+        className={className({
+          "player": true,
+          'video-mute': options.user.videoTrack && options.user.videoMuted,
+          'audio-mute': options.user.audioTrack && options.user.audioMuted
+        })}>
+      </div>
+
+      <div className="info">
+        <span>{ attendee?.name }</span>
         <div className={className({ "audio-status": true, "mute": options.user.audioTrack?.getStats().muteState })}>
           { (options.user.audioTrack && !options.user.audioMuted) && <AudioVolume track={ options.user.audioTrack } /> }
           { (options.user.audioTrack && options.user.audioMuted) && <MicrophoneSlashSVG height={18} /> }
