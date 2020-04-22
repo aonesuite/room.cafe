a36bfed895021c2b6985a155009d8f529032bdf9
Miclle Zheng <miclle.zheng@gmail.com>
2020-04-22 22:19:45 +0800

    Remove vue website app

--- website/src/components/icons/ScreenExit.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8">
-    <path fill="currentColor" d="M1 0l-1 1 1.5 1.5-1.5 1.5h4v-4l-1.5 1.5-1.5-1.5zm3 4v4l1.5-1.5 1.5 1.5 1-1-1.5-1.5 1.5-1.5h-4z"></path>
-  </svg>
-</template>
